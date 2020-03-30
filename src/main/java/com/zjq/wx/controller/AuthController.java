ca27f64b643e34bb472624214cda0189d4f9180d
BadGuy523 <694255828@qq.com>
2020-03-30 17:07:21 +0800

    ADD:jenkins自动化部署测试03

--- src/main/java/com/zjq/wx/controller/AuthController.java
+++ src/main/java/com/zjq/wx/controller/AuthController.java
@@ -79,7 +79,7 @@ public class AuthController {
         log.info("--FromUserName:"+request.getParameter("FromUserName"));
         log.info("--openId:"+openId);
         //return DecryptUtil.getResponse(request.getParameter("nonce"),request.getParameter("openid"));
-        return "<xml><ToUserName><![CDATA["+fromUserName+"]]></ToUserName><FromUserName><![CDATA["+toUserName+"]]></FromUserName><CreateTime>12345678</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[jenkins更新成功2]]></Content></xml>";
+        return "<xml><ToUserName><![CDATA["+fromUserName+"]]></ToUserName><FromUserName><![CDATA["+toUserName+"]]></FromUserName><CreateTime>12345678</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[jenkins更新成功3]]></Content></xml>";
     }
 
 }
